f909bba946bd2e015c8fb080fe6c489eea4916aa
Tuna <celena.toon@agenusbio.com>
2018-08-03 12:04:17 -0400

    Added recommendations experiments

--- air-ml-prediction.py
+++ air-ml-prediction.py
@@ -2,6 +2,9 @@
 import boto3
 import pickle
 from io import BytesIO
+import requests
+import numpy as np
+import pandas as pd
 
 '''
 JSON Request
@@ -24,25 +27,32 @@ def main(event, context):
     elif event['type'] == 'Feedly':
       type_feedly = 1
 
-    # Create matrix to be sent to AWS SageMaker
-    matrix = [[
-      event['likes_cnt'],
-      event['comments_cnt'],
-      event['storyboard_cnt'],
-      type_document,
-      type_feedly
-    ]]
+    # Create data to be sent to AWS SageMaker
+    predict = {
+      'likes_cnt': event['likes_cnt'],
+      'comments_cnt': event['comments_cnt'],
+      'storyboard_cnt': event['storyboard_cnt'],
+      'type_document': type_document,
+      'type_feedly': type_feedly
+    }
 
     # Load TFIDF pickle and get values for incoming text
     s3 = boto3.client('s3')
     response = s3.get_object(Bucket='air-machine-learning', Key='tfidf.pkl')
     pickled_tfidf = response['Body'].read()
     tfidf_vectorizer = pickle.loads(pickled_tfidf)
-    transformed_title = tfidf_vectorizer.transform(event['title'])
+    transformed_title = tfidf_vectorizer.transform([event['title']])
 
-    print('matrix: \n', matrix)
-    print('tfidf: \n', tfidf_vectorizer)
-    print('transformed title: \n', transformed_title)
+    new_df = pd.DataFrame(predict)
+    tfidf_df = pd.DataFrame(transformed_title.todense())
+    final_df = pd.concat([new_df, tfidf_df], axis=1)
+
+    # send request to Sagemaker
+    sagemaker_endpoint = 'https://runtime.sagemaker.us-east-1.amazonaws.com/endpoints/kmeans-2018-06-05-14-38-21-961/invocations'
+    r = requests.post(sagemaker_endpoint, data={ 'body': final_df.as_matrix().astype(np.float32) })
+
+    print(r)
+    return r
 
     # sagemaker = boto3.client('sagemaker')
     # try:
